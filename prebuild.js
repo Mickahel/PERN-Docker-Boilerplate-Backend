97311ac98fff6edd3cf336257d9c0cd261a5a9c6
Mickahel <df.michelangelo@gmail.com>
2021-02-21 20:20:02 +0100

    added the condition for git existence

--- prebuild.js
+++ prebuild.js
@@ -1,19 +1,26 @@
 const { execSync } = require('child_process')
 const fs = require("fs")
-
-console.log("Getting GIT data")
-
-const out = {}
-out.commit = execSync('git rev-parse --short HEAD').toString().replace("\n","")
-//out.tag = execSync('git describe --abbrev=0 --tags').toString().replace("\n","")
-out.date = execSync('git show -s --format=%cd --date=local').toString().replace("\n","")
-
-fs.writeFileSync('./src/version.json', JSON.stringify(out))
-console.log("Deleting build folder")
-try{
-    execSync('rimraf build/*')
-}catch(e){
-    console.log(e)
+const path = require("path")
+function isGitSync(dir) {
+    return fs.existsSync(path.join(dir, '.git'))
 }
 
-console.log("Finished, starting build")
+
+if (isGitSync(".")) {
+    console.log("Getting GIT data")
+
+    const out = {}
+    out.commit = execSync('git rev-parse --short HEAD').toString().replace("\n", "")
+    //out.tag = execSync('git describe --abbrev=0 --tags').toString().replace("\n","")
+    out.date = execSync('git show -s --format=%cd --date=local').toString().replace("\n", "")
+
+    fs.writeFileSync('./src/version.json', JSON.stringify(out))
+    console.log("Deleting build folder")
+    try {
+        execSync('rimraf build/*')
+    } catch (e) {
+        console.log(e)
+    }
+
+    console.log("Finished, starting build")
+}
